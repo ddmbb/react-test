2d223dc461bfaaa0afd97c5f02a6054df4115b1d
ddmbb <d.baker089@gmail.com>
2020-05-14 16:51:41 -0400

    roulette updated

--- src/state-drills/RouletteGun.js
+++ src/state-drills/RouletteGun.js
@@ -14,34 +14,22 @@ class RouletteGun extends React.Component {
         spinningTheChamber: true,
      });
      setTimeout (() => {
-         let randomChamber = null;
-         randomNumber(1, 8){
-             randomChamber = Math.random() * 8;
+         let randomChamber = (Math.random() * 8);
         console.log(randomChamber);
-         } }, 2000);
-     });
-  };
+         }, 2000);
+    this.setState({
+        spinningTheChamber: false,
+    });
+     }
+  }
 
-//   componentDidMount() {
-//     console.log("componentDidMount");
-//     this.interval = setInterval(() => {
-//       console.log("setInterval");
-//       this.setState({ count: this.state.chamber  });
-//     }, 2000);
-//   }
-
-//   componentWillUnmount() {
-//     clearInterval(this.interval);
-//   }
-
-  render() {
+render() {
     let result = null;
-    if (this.state.chamber === null && this.state.spinningTheChamber === false) result = <p>"nothing"</p>;
+    if (this.state.chamber === null && this.state.spinningTheChamber === false) result = <button onClick={() => this.handleButtonClick("Chamber")}>Pull the trigger!</button>;
     else if (this.state.spinningTheChamber === true) result = <p>"spinning the chamber and pulling the trigger!"</p>;
     else if (this.state.chamber === randomChamber && this.state.spinningTheChamber === false) result = <p>"BANG!!!"</p>;
     else if (this.state.chamber !== randomChamber && this.state.spinningTheChamber === false) result = <p>you're safe!</p>;
     return <div>{result}</div>;
-  }
-}
+  };
 
 export default RouletteGun;
